1d84ab4a80b7768065363b261c6cfeb91332a42f
tro-lee <1481895142@qq.com>
2024-04-08 15:45:29 +0800

    .gitignore

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import antfu from '@antfu/eslint-config';
-// export default [
-//   {languageOptions: { globals: globals.browser }},
-//   pluginJs.configs.recommended,
-//   ...tseslint.configs.recommended,
-// ];
-
-export default antfu();
