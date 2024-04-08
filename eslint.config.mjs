dd267c74af72b212de8202ce669a96a70a256cdf
tro-lee <1481895142@qq.com>
2024-04-08 15:37:27 +0800

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import antfu from '@antfu/eslint-config';
+// export default [
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+// ];
+
+export default antfu();
